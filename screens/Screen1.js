cee4196c0ddb6b15d96493bec249603bca461dba
teacher16092 <teacherwh.jisha@gmail.com>
2021-03-18 14:08:59 +0530

    commit changes

---
+++ screens/Screen1.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View , TouchableOpacity} from 'react-native';
+
+
+export default class Screen1 extends React.Component{
+    pressMe=()=> {
+        this.props.navigation.navigate("Screen2")
+      }
+
+    render()
+    {
+      return(
+          <View style= {{marginTop: 100, alignSelf: "center"}}>
+              <TouchableOpacity onPress={() => {this.pressMe()}}> 
+                  
+                  <Text>
+                      Screen1
+                  </Text>
+              </TouchableOpacity>
+          </View>
+      )
+    }
+  }
