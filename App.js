80c826f57c2664e6b4a89cc7f55f8da38693a973
teacher16092 <teacherwh.jisha@gmail.com>
2021-03-18 13:54:35 +0530

    commit changes

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createSwitchNavigator,createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import Screen1 from './screens/Screen1';
+import Screen2 from './screens/Screen2';
+import {Icon} from  "react-native-elements";
+import "react-native-vector-icons"
+// this commented out code is the default code
 
+/*
 export default function App() {
   return (
     <View style={styles.container}>
@@ -9,7 +17,16 @@ export default function App() {
       <StatusBar style="auto" />
     </View>
   );
+}*/
+
+export default class App extends React.Component{
+  
+  render()
+  {
+    return <AppContainer/>
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +36,40 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+/* commenting the code as to use TabNavigator
+const switchN = createSwitchNavigator({
+  Screen1 :{screen:Screen1},
+  Screen2 :{screen:Screen2}
+})
+
+*/
+
+const switchN = createBottomTabNavigator(
+{
+  Screen1 :{screen:Screen1},
+  Screen2 :{screen:Screen2}
+},
+{
+  defaultNavigationOptions : ({navigation}) => ({
+  tabBarIcon :({tintColor}) => {
+    const routeName = navigation.state.routeName;
+    if(routeName == "Screen1")
+    {
+      return < Icon name = "home" type ="entypo" color = {tintColor}/>; 
+    }
+ else if (routeName == "Screen2"){
+<image  source = {require('./assets/favicon.png')}
+style = {{width:30,height:30}} />
+
+ };
+  }
+},
+tabBarOptions : {
+  activeTintColor : "#000708",
+  inactiveTintColor : "#030303",
+},
+  )
+},
+);
+const AppContainer = createAppContainer(switchN)
